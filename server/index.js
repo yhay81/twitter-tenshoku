1cc04741273245121ac3c4ffb4765dd933e8dfdb
Yusuke Hayashi <yusuke8h@gmail.com>
2018-05-15 18:09:38 +0900

    Make Twitter auth work

--- server/index.js
+++ server/index.js
@@ -5,9 +5,12 @@ const path = require("path");
 const serveStatic = require("serve-static");
 const session = require("express-session");
 const resumeFactory = require("./resumeFactory");
+const url = require("url");
+
+require("dotenv").config();
 
 const passport = require("passport");
-const TwitterStrategy = require("passport-twitter").Strategy;
+const { Strategy } = require("passport-twitter");
 
 const TWITTER_CONSUMER_KEY = process.env.TWITTER_CONSUMER_KEY;
 const TWITTER_CONSUMER_SECRET = process.env.TWITTER_CONSUMER_SECRET;
@@ -28,7 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
-    secret: "keyboard cat",
+    secret: "typing-cat",
     resave: true,
     saveUninitialized: true
   })
@@ -40,16 +43,24 @@ app.use("/", serveStatic(path.join(__dirname, "..")));
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.serializeUser((user, callback) => {
+  callback(null, user);
+});
+
+passport.deserializeUser((obj, callback) => {
+  callback(null, obj);
+});
+
 // passport-twitterの初期化
 passport.use(
-  new TwitterStrategy(
+  new Strategy(
     {
-      consumerKey: TWITTER_CONSUMER_KEY, //TwitterのconsumerKey
-      consumerSecret: TWITTER_CONSUMER_SECRET, //TwitterのconsumerSecret
-      callbackURL: BASE_URL + "/auth/twitter/callback" //認証成功時の戻り先URL
+      consumerKey: TWITTER_CONSUMER_KEY,
+      consumerSecret: TWITTER_CONSUMER_SECRET,
+      callbackURL: url.resolve(BASE_URL, "/auth/twitter/callback")
     },
-    (token, tokenSecret, profile, cb) => {
-      cb(profile);
+    (token, tokenSecret, profile, callback) => {
+      return callback(null, profile);
     }
   )
 );
@@ -58,20 +69,27 @@ app.get("/auth/twitter", passport.authenticate("twitter"));
 
 app.get(
   "/auth/twitter/callback",
-  passport.authenticate("twitter", {
-    failureRedirect: "/"
-  }),
+  passport.authenticate("twitter", { failureRedirect: "/fail" }),
   (req, res) => {
+    const { oauth_token, oauth_verifier } = req.query;
     res.redirect("/");
   }
 );
+
+app.get("/fail", async (req, res) => {
+  try {
+    res.send(200, "auth fail");
+  } catch (err) {
+    console.error("Error loading locations!", err);
+    res.send(500, "Internal server error");
+  }
+});
 
 app.post("/api/", async (req, res) => {
   try {
     const canvas = await resumeFactory(req.body.text);
     res.setHeader("Content-Type", "image/png");
     canvas.pngStream().pipe(res);
-    // res.status(200).send(JSON.stringify(canvas));
   } catch (err) {
     console.error("Error loading locations!", err);
     res.send(500, "Internal server error");
@@ -80,6 +98,7 @@ app.post("/api/", async (req, res) => {
 
 // Always return the main index.html, so react-router render the route in the client
 app.get("*", (req, res) => {
+  console.log("/", req.session.passport);
   res.sendFile(path.resolve(__dirname, "..", "index.html"));
 });
 
